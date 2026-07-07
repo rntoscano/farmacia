e572f71ac076e8021844ecbb25d725909d3b676a
rntoscano <anny.toscano@hotmail.com.com>
2026-07-07 11:44:29 -0300

    Componentes atualização

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './pages/footer/Footerr';
-import Navbar from './pages/navbar/Navbar';
+import Footer from './components/footer/Footerr';
+import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
 
 function App() {
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <>
+      <div className="w-full bg-indigo-900 py-4 text-white">
+        <div className="container mx-auto flex justify-between">
+          <Link to="/home" className="text-lg font-bold">
+  Farmácia
+</Link>
+
+          <div className="flex gap-4">
+            <div>Categorias</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
